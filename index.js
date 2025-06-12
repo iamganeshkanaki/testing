05fe2fb05d967deede531b7d4e124b0f11a125e9
iamganeshkanaki <iamganeshkanaki@gmail.com>
2025-06-13 00:53:41 +0530

    changed

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const port = 3001;
 
 // PostgreSQL setup
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "0000",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // Middleware
